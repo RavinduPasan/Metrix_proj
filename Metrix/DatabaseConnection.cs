9c84925eecb7652405a8f2f9de6a4c4ad6fd5d84
rav94 <fernandoravindu1994@gmail.com>
2014-12-01 01:00:20 +0530

    DB Class corrected

--- Metrix/DatabaseConnection.cs
+++ Metrix/DatabaseConnection.cs
@@ -3,55 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace Metrix
 {
-    class DatabaseConnection
-    {
-        private string sql_string; //sql string
-        private string strCon;  //connection url
-        System.Data.SqlClient.SqlDataAdapter da_1; //opening the table in db - data adapter
+    class DatabaseConnection{
 
-        public string Sql { //Property
-            
-            set { sql_string = value; }
+        private MySqlConnection conn; //opening a connection
+        private string server; 
+        private string db; //database name
+        private string uid; //username
+        private string pass; //password
+
+        //Constructer
+        public DatabaseConnection() {
+
+            Initialize();    
         
         }
 
-        public string connection_string { //Property
-            
-            set { strCon = value; }
+        //initialize values
+        private void Initialize() {
+
+            server = "localhost";
+            db = "metrix";
+            uid = "root";
+            pass = "";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            db + ";" + "UID=" + uid + ";" + "PASSWORD=" + pass + ";";
+
+            conn = new MySqlConnection(connectionString);
+        
         
         }
-
-        //Loads the table into the DataSet
-        //Dataset does that
-        public System.Data.DataSet GetConnection { //Read-only property
-            
-            get { return MyDataSet(); } //connects to the database and fill a dataset
-        
-        }
-
-        private System.Data.DataSet MyDataSet() {
-
-            System.Data.SqlClient.SqlConnection con = new System.Data.SqlClient.SqlConnection(strCon); //connection object
-
-            con.Open();
-
-            //1st param - record want from db , 2nd param- con obj - which db table is in
-            da_1 = new System.Data.SqlClient.SqlDataAdapter(sql_string, con);
-
-            //whole method return this data set, hold the all records from db
-            System.Data.DataSet dat_set = new System.Data.DataSet(); 
-            da_1.Fill(dat_set, "Table_Data_1"); //fill the dat set - after comma can add a name for the fill
-            con.Close();
-
-            return dat_set;
-
-            
-        
-        }
- 
 
     }
 }
